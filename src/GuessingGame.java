be7305d8954b8a357c6a0e231ecf2e6960e722f3
anonym-266 <desmondtardzenyuy@gmail.com>
2025-04-26 16:10:02 +0100

    refactored code to use constants instead of hardcoding attempts

--- src/GuessingGame.java
+++ src/GuessingGame.java
@@ -2,26 +2,33 @@ import java.util.Scanner;
 import java.util.Random;
 
 public class GuessingGame {
+    private static final int MAX_ATTEMPS = 3;
+
     public static void main(String[] args) {
+
+        System.out.println(Colors.YELLOW + Colors.BOLD + "WELCOME TO GUESSING GAME" + Colors.RESET);
+
+
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
         int randomNumber = random.nextInt(10);
         System.out.println(randomNumber);
 
-        for (int i = 0; i <= 3; i++) {
+        for (int i = 0; i <= MAX_ATTEMPS; i++) {
             System.out.print(Colors.BLUE + "Enter the number:  " + Colors.RESET);
             int userInput = scanner.nextInt();
             if (userInput == randomNumber) {
                 System.out.println(Colors.GREEN + "you win" + Colors.RESET);
                 break;
             } else {
-                if (i < 3) {
+                if (i < MAX_ATTEMPS) {
                     if (userInput > randomNumber) {
-                        System.out.println(Colors.RED + "Too big, " + Colors.BOLD + (3 - i) + Colors.RESET + Colors.RED
+                        System.out.println(Colors.RED + "Too big, " + Colors.BOLD + (MAX_ATTEMPS - i) + Colors.RESET + Colors.RED
                                 + " tries left" + Colors.RESET);
                     } else {
-                        System.out.println(Colors.RED + "Too small, " + Colors.BOLD + (3 - i) + Colors.RESET + Colors.RED
-                                + " tries left" + Colors.RESET);
+                        System.out
+                                .println(Colors.RED + "Too small, " + Colors.BOLD + (MAX_ATTEMPS - i) + Colors.RESET + Colors.RED
+                                        + " tries left" + Colors.RESET);
                     }
                 } else {
                     System.out.println(Colors.RED + "you loose, correct guess was: " + Colors.BOLD + randomNumber
