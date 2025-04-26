e8f553a3d318d10b44cda8c6e486727bbbe1fde5
anonym-266 <desmondtardzenyuy@gmail.com>
2025-04-26 21:16:23 +0100

    added functionality to keep record of user results

--- src/GuessingGame.java
+++ src/GuessingGame.java
@@ -1,25 +1,20 @@
 import java.util.Scanner;
-import java.util.Random;
 
 public class GuessingGame {
-    private static final int MAX_ATTEMPS = 3;
+    private static final int MAX_ATTEMPTS = 3;
 
     public static void main(String[] args) {
 
         System.out.println(Colors.YELLOW + Colors.BOLD + "WELCOME TO GUESSING GAME" + Colors.RESET);
 
         Scanner scanner = new Scanner(System.in);
-        Random random = new Random();
-
-        int randomNumber = random.nextInt(10);
-        System.out.println(randomNumber);
-
-        MainGame.play(MAX_ATTEMPS, randomNumber, scanner);
+        
+        MainGame.playAndSave(MAX_ATTEMPTS, scanner);
 
         boolean continueResponse = GetValidInput.getValidContinueResponse(scanner);
 
         if (continueResponse) {
-            MainGame.play(MAX_ATTEMPS, randomNumber, scanner);
+            MainGame.playAndSave(MAX_ATTEMPTS, scanner);
         } else {
             System.out.println("Exited");
         }
--- src/MainGame.java
+++ src/MainGame.java
@@ -1,30 +1,58 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Random;
 import java.util.Scanner;
 
 public class MainGame {
-  public static void play(int MAX_ATTEMPS, int randomNumber, Scanner scanner) {
-    for (int i = 0; i <= MAX_ATTEMPS; i++) {
-      System.out.print(Colors.BLUE + "Enter the number:  " + Colors.RESET);
-      int userInput = GetValidInput.getValidNumber(scanner);
-      if (userInput == randomNumber) {
-          System.out.println(Colors.GREEN + "you win" + Colors.RESET);
-          break;
-      } else {
-          if (i < MAX_ATTEMPS) {
-              if (userInput > randomNumber) {
-                  System.out.println(
-                          Colors.RED + "Too big, " + Colors.BOLD + (MAX_ATTEMPS - i) + Colors.RESET + Colors.RED
-                                  + " tries left" + Colors.RESET);
-              } else {
-                  System.out
-                          .println(Colors.RED + "Too small, " + Colors.BOLD + (MAX_ATTEMPS - i) + Colors.RESET
-                                  + Colors.RED
-                                  + " tries left" + Colors.RESET);
-              }
-          } else {
-              System.out.println(Colors.RED + "you loose, correct guess was: " + Colors.BOLD + randomNumber
-                      + Colors.RESET + Colors.RESET);
-          }
-      }
-  }
-  }
+    public static String play(int MAX_ATTEMPTS, Scanner scanner) {
+        Random random = new Random();
+
+        int randomNumber = random.nextInt(10);
+        System.out.println(randomNumber);
+
+        int numberOfAttemptsMade = 0;
+        for (int i = 0; i <= MAX_ATTEMPTS; i++) {
+            System.out.print(Colors.BLUE + "Enter the number:  " + Colors.RESET);
+            int userInput = GetValidInput.getValidNumber(scanner);
+            if (userInput == randomNumber) {
+                System.out.println(Colors.GREEN + "you win" + Colors.RESET);
+                break;
+            } else {
+                if (i < MAX_ATTEMPTS) {
+                    if (userInput > randomNumber) {
+                        System.out.println(
+                                Colors.RED + "Too big, " + Colors.BOLD + (MAX_ATTEMPTS - i) + Colors.RESET + Colors.RED
+                                        + " tries left" + Colors.RESET);
+                    } else {
+                        System.out
+                                .println(Colors.RED + "Too small, " + Colors.BOLD + (MAX_ATTEMPTS - i) + Colors.RESET
+                                        + Colors.RED
+                                        + " tries left" + Colors.RESET);
+                    }
+                } else {
+                    System.out.println(Colors.RED + "you loose, correct guess was: " + Colors.BOLD + randomNumber
+                            + Colors.RESET + Colors.RESET);
+                }
+            }
+            numberOfAttemptsMade = i;
+        }
+        String gameResult = String.format("Succeeded in %d attempts", numberOfAttemptsMade + 1);
+        System.out.println(gameResult);
+        return gameResult;
+    }
+
+    public static void playAndSave(int MAX_ATTEMPTS, Scanner scanner) {
+        String gameResults = MainGame.play(MAX_ATTEMPTS, scanner);
+
+        try {
+            final Path path = Files.createTempFile("gameResults", ".log");
+            Files.write(path, gameResults.getBytes());
+            path.toFile().deleteOnExit();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
