b73f16c87d622392788ea354c949213eb396f9b1
anonym-266 <desmondtardzenyuy@gmail.com>
2025-04-28 08:10:25 +0100

    added documentation for menu function

--- src/Menu.java
+++ src/Menu.java
@@ -4,6 +4,9 @@ import java.util.Scanner;
 public class Menu {
     private static Path lastGameResult = null;
 
+    /**
+     * Prints menu for user to make choice
+     */
     public static void printMenu() {
         System.out.println(Colors.CYAN + "\n======== MENU ========" + Colors.RESET);
         System.out.println(Colors.GREEN + "1." + Colors.RESET + " Play game");
@@ -11,6 +14,13 @@ public class Menu {
         System.out.println(Colors.CYAN + "=======================" + Colors.RESET);
     }
 
+    /**
+     * Handles user input
+     * 
+     * @param MAX_ATTEMPTS the maximum number of attempts allowed
+     * @param scanner      scanner object used to read user input from standard
+     *                     input
+     */
     public static void menuChoice(int MAX_ATTEMPTS, Scanner scanner) {
         System.out.print(Colors.BLUE + "Enter your choice: " + Colors.RESET);
         int choice = GetValidInput.getValidNumber(scanner);
@@ -20,11 +30,12 @@ public class Menu {
                 lastGameResult = MainGame.playAndSave(MAX_ATTEMPTS, scanner);
                 break;
             case 2:
-            if (lastGameResult == null) {
-                System.out.println(Colors.RED + "\nNo game results found. Please play a game first." + Colors.RESET);
-            } else {
-                MainGame.readResults(lastGameResult);
-            }
+                if (lastGameResult == null) {
+                    System.out
+                            .println(Colors.RED + "\nNo game results found. Please play a game first." + Colors.RESET);
+                } else {
+                    MainGame.readResults(lastGameResult);
+                }
                 break;
             default:
                 System.out.println(Colors.RED + "Invalid choice. Please select 1 or 2." + Colors.RESET);
